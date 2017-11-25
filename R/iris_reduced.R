85c5cedaaf6bbf7e28cf6d592194970f2f3a0c10
Peter Kamerman <peter.kamerman@gmail.com>
2017-11-25 19:54:04 +0200

    Added dataset documentation

---
+++ R/iris_reduced.R
@@ -0,0 +1,16 @@
+#' @title Simplified version of the 'iris' dataset.
+#'
+#' @description This is a simplified version of the (Fisher or Anderson) \emph{iris} dataset.
+#'
+#' @format A data frame data frame with 100 cases (rows) and 5 variables (columns):
+#' \describe{
+#'   \item{Sepal.Length}{Iris flower sepal length in centimeters.}
+#'   \item{Sepal.Width}{Iris flower sepal width in centimeters.}
+#'   \item{Petal.Length}{Iris flower petal length in centimeters.}
+#'   \item{Petal.Width}{Iris flower petal width in centimeters.}
+#'   \item{Species}{Species of iris in the dataset: \emph{iris setosa} and \emph{iris versicolor}.}
+#' }
+#'
+#' @source \code{\link[datasets]{iris}}
+
+'iris_reduced'
