a18df1bc924bdb23e8601d4821c4669f067887a5
Peter Kamerman <peter.kamerman@gmail.com>
2017-11-27 22:14:55 +0200

    Test boot_statistics.R functions

---
+++ tests/testthat/test-boot-statistics.R
@@ -0,0 +1,90 @@
+library(easyboot)
+
+test_that("Assess whether 'boot' statistic functions work as expected", {
+
+    # Generate univariate data (including an NA value)
+    foo <- data.frame(data = c(NA, iris[1:50, 'Petal.Width']))
+    bar <- data.frame(data = iris[1:80, 'Species'])
+    bar$data <- as.character(bar$data)
+    bar$data[[1]] <- NA
+    # Generate bivariate data (including an NA value)
+    baz <- iris[1:100, c('Petal.Width', 'Species')]
+    baz$Species <- as.character(baz$Species)
+    baz$Petal.Width[[1]] <- NA
+    qux <- baz
+    qux$Petal.Width[qux$Petal.Width < 1] <- 'small'
+    qux$Petal.Width[grepl('1.?.?', qux$Petal.Width)] <- 'large'
+
+    # Test boot_mean
+    expect_equal(object = boot::boot(data = foo,
+                                     statistic = boot_mean,
+                                     R = 1000,
+                                     stype = 'i')$t0,
+                 expected = 0.246,
+                 tolerance = 0.001)
+
+    # Test boot_geometric
+    expect_equal(object = boot::boot(data = foo,
+                                     statistic = boot_geometric,
+                                     R = 1000,
+                                     stype = 'i')$t0,
+                 expected = 0.2265819,
+                 tolerance = 0.001)
+
+    # Test boot_median
+    expect_equal(object = boot::boot(data = foo,
+                                     statistic = boot_median,
+                                     R = 1000,
+                                     stype = 'i')$t0,
+                 expected = median(foo$data,
+                                   na.rm = TRUE),
+                 tolerance = 0.001)
+
+    # Test boot_mode
+    expect_equal(object = boot::boot(data = foo,
+                                     statistic = boot_mode,
+                                     R = 1000,
+                                     stype = 'i')$t0,
+                 expected = 0.2,
+                 tolerance = 0.001)
+
+    # Test boot_prop
+    expect_equal(object = boot::boot(data = bar,
+                                     statistic = boot_prop,
+                                     R = 1000,
+                                     stype = 'i')$t0,
+                 expected = 0.6202532,
+                 tolerance = 0.001)
+
+    # Test difference between arithmetic means
+    expect_equal(object = boot::boot(data = baz,
+                                     statistic = boot_delta_mean,
+                                     R = 1000,
+                                     stype = 'i')$t0,
+                 expected = -1.079061,
+                 tolerance = 0.001)
+
+    # Test the difference between geometric means
+    expect_equal(object = boot::boot(data = baz,
+                                     statistic = boot_delta_geometric,
+                                     R = 1000,
+                                     stype = 'i')$t0,
+                 expected = -1.084028,
+                 tolerance = 0.001)
+
+    # Test the difference between medians
+    expect_equal(object = boot::boot(data = baz,
+                                     statistic = boot_delta_median,
+                                     R = 1000,
+                                     stype = 'i')$t0,
+                 expected = -1.1,
+                 tolerance = 0.001)
+
+    # Test the odds ratio
+    expect_equal(object = boot::boot(data = qux,
+                                     statistic = boot_OR,
+                                     R = 1000,
+                                     stype = 'i')$t0,
+                 expected = 0,
+                 tolerance = 0.001)
+    })
