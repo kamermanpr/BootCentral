a81a629301c4635272263ebea861351cb66049c3
Peter Kamerman <peter.kamerman@gmail.com>
2017-11-28 17:12:45 +0200

    Added harmonic mean and delta harmonic mean

--- R/boot_statistics.R
+++ R/boot_statistics.R
@@ -1,19 +1,18 @@
-#' @title `boot` statistic functions
+#' @title 'boot' statistic functions
 #'
-#' @description A series of functions for internal use within \code{\link[boot]{boot}} only. The functions calculate measures of central tendency (arithmetic mean, geometric mean, median, and mode), differences in measures of central tendency (difference in means, difference in geometric means, difference in medians), proportions, and odds ratios.
+#' @description A series of functions for internal use within \code{\link[boot]{boot}} only. The functions calculate measures of central tendency (arithmetic mean, geometric mean, harmonic mean, median, and mode), differences in measures of central tendency (difference in arithmetic means, difference in geometric means, difference in harmonic means, and difference in medians), proportions, and odds ratios.
 #'
 #' @param d A data frame.
 #'
-#'    For `boot_mean`, `boot_geometric`, `boot_median`, and `boot_mode`, the data frame must consist of a single column of numeric data.
+#'    For `boot_mean`, `boot_geometric`, `boot_harmonic`, `boot_median`, and `boot_mode`, the data frame must consist of a single column of numeric data \emph{(dimensions: n x 1)}.
 #'
-#'    For `boot_prop`, the data frame must consist of a single column of binomial categorical data.
+#'    For `boot_prop`, the data frame must consist of a single column of binomial categorical data \emph{(dimensions: n x 1)}.
 #'
-#'    For `boot_delta_mean`, `boot_delta_geometric`, and `boot_delta_median`, the data frame must consist of a two columns. The first column must be numeric data, and the second column must be a two-level grouping variable.
+#'    For `boot_delta_mean`, `boot_delta_geometric`, `boot_delta_harmonic`, and `boot_delta_median`, the data frame must consist of a two columns. The first column must be numeric data, and the second column must be a two-level grouping variable \emph{(dimensions: n x 2)}.
 #'
-#'    For `boot_OR` the data frame must consist of a two columns. The first column must be binomial categorical data, and the second column must be a two-level grouping variable.
+#'    For `boot_OR` the data frame must consist of a two columns. The first column must be binomial categorical data, and the second column must be a two-level grouping variable \emph{(dimensions: n x 2)}.
 #'
-#' @param i Internal indexing parameter used by \code{\link[boot]{boot}} for resampling.
-#' \strong{Do not change}.
+#' @param i Internal indexing parameter used by \code{\link[boot]{boot}} for resampling. \strong{Do not change}.
 #
 #' @return Returns and object of class "boot".
 #'
@@ -38,6 +37,17 @@ boot_geometric <- function(d, i){
     # Calculate geometric mean
     n <- length(df)
     prod(df)^(1/n)
+}
+
+#' @describeIn boot_mean Calculates a harmonic mean.
+#' @export
+boot_harmonic <- function(d, i){
+    # Sample
+    df <- d[i, ]
+    # Remove NA
+    df <- df[!is.na(df)]
+    # Calculate harmonic mean
+    1 / mean(1 / df)
 }
 
 #' @describeIn boot_mean Calculates a median
@@ -107,6 +117,20 @@ boot_delta_geometric <- function(d, i){
     geo_1 <- prod(df_1)^(1/n_1)
     geo_2 <- prod(df_2)^(1/n_2)
     geo_1 - geo_2
+}
+
+#' @describeIn boot_mean Calculates the difference between harmonic means
+#' @export
+boot_delta_harmonic <- function(d, i){
+    # Sample
+    df <- d[i, ]
+    # Remove NA
+    df <- df[complete.cases(df), ]
+    # Get unique values for grouping variable
+    uniq <- unique(df[[2]])
+    # Calculate delta means
+    (1 / mean(1/ df[1][df[2] == uniq[[1]]])) -
+        (1 / mean(1 / df[1][df[2] == uniq[[2]]]))
 }
 
 #' @describeIn boot_mean Calculates the difference between medians
